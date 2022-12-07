61ea1c08addfce7112493161e64171fc21b3753a
Corbin Cutter <Corbin@Lisas-iMac.lan>
2022-12-06 23:33:52 -0500

    Routes done?

--- server.js
+++ server.js
@@ -1,27 +1,60 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3001;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// Middleware below
+app.use(express.static('public'));
+
+
+// get routes
 
 app.get('/', (req, res) => {
-
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.post('/', (req, res) => {
-
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'notes.html'));
 });
 
-app.delete('/', (req, res) => {
+app.post('/db', (req, res) => {
 
+  console.info(`${req.method} request received!`);
+
+  let response;
+
+  if (req.body && req.body.note) {
+    response = {
+      status: 'success',
+      data: req.body,
+    };
+    res.json(`Your ${response.data.note} has been added!`);
+  } else {
+    res.json('Request body must at least contain a note!');
+  }
+
+  // Log the response body to the console
+  console.log(req.body);
 });
 
-app.put('/', (req, res) => {
 
-});
+// More routes below just in case
+
+// app.post('/', (req, res) => {
+
+// });
+
+// app.delete('/', (req, res) => {
+
+// });
+
+// app.put('/', (req, res) => {
+
+// });
 
 
 app.listen(PORT, () =>
-  console.log(`Note Taker app listening at http://localhost:${PORT}`)
+  console.log(`Note Taker app listening at http://localhost:${3001}`)
 );
