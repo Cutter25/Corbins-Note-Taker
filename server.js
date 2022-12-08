089bdb390accc5a2aadb61654b3da82dc5ee9206
Corbin Cutter <Corbin@Lisas-iMac.lan>
2022-12-08 18:30:13 -0500

    Troubleshooting

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { response } = require('express');
 const express = require('express');
 const app = express();
 const path = require('path');
-const notes = require('./Develop/db/db.json');
+const notes = require('./db/db.json');
 const PORT = process.env.port || 3001;
 
 app.use(express.urlencoded({ extended: true }));
@@ -13,17 +13,17 @@ app.use(express.json());
 
 // express middleware for static assets (front-end files) in the public directory!
 
-app.use(express.static('Develop'));
+app.use(express.static('public'));
 
 
 // get routes!
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Develop/public/notes.html'))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
 // get route that matches the fetch from the front-end! (index.js) (basically our own API call) :)
