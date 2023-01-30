cd926dda9fe6cb8170e5561346446610be431e63
Corbin Cutter <Corbin@Lisas-iMac.lan>
2023-01-29 20:40:33 -0500

    api routes done

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,25 @@
 // Requiring
 const router = require('express').Router();
 const store = require('../db/store')
+
+// GET all notes
+router.get("/notes", (req, res) => {
+    store.getNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err));
+});
+
+// POST for new note
+router.post("/notes", (req, res) => {
+    store.saveNote(req.body)
+        .then((note) => res.json(note))
+        .catch(err => res.status(500).json(err));
+});
+
+// DELETE route to delete notes based on a unique id
+router.delete("/notes/:id", (req, res) => {
+    store.deleteNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        console.log('Note successfully deleted!')
+        .catch(err => res.status(500).json(err));
+});
